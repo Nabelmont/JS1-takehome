02a0c3e40a8e59480a18a4f8879c977a930a4777
Noah <nabelmont@me.com>
2017-11-01 18:25:35 -0700

    tryign to push but error on pushing

---
+++ js/scripts.js
@@ -0,0 +1,2 @@
+var message = "I hope to learn javascript in this class, then take the angular class. I would also like so learn react.js, because I am interested in builiding ios applications";
+console.log(message);
