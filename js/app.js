a3f105f8249090edc5966bb6a017daa5e79a5c20
Noah <nabelmont@me.com>
2017-11-06 12:48:33 -0800

    Added how I think take home is done

---
+++ js/app.js
@@ -0,0 +1,11 @@
+var nameFirst = prompt("Enter your First Name.");
+
+console.log(nameFirst);
+
+var nameLast = alert("Enter Last Name");
+
+var userBirthday = confirm("Enter Birthday");
+
+var alertBirthday = alert(userBirthday);
+
+console.log(nameLast, userBirthday);
--- js/scripts.js
+++
@@ -1,2 +0,0 @@
-var message = "I hope to learn javascript in this class, then take the angular class. I would also like so learn react.js, because I am interested in builiding ios applications";
-console.log(message);
