644709d7b8134a1e4c692087e89bc52484c23ad7
Noah <nabelmont@me.com>
2017-11-06 18:07:25 -0800

    corrected take home to work

--- js/app.js
+++ js/app.js
@@ -1,11 +1,20 @@
-var nameFirst = prompt("Enter your First Name.");
+// var nameFirst = prompt("Enter your First Name.");
+//
+// console.log(nameFirst);
+//
+// var nameLast = alert("Enter Last Name");
+//
+// var userBirthday = confirm("Enter Birthday"Date());
+//
+// var alertBirthday = alert(userBirthday);
+//
+// console.log(nameLast, userBirthday);
 
-console.log(nameFirst);
+var firstName = prompt('Whats your first name');
+console.log(firstName);
 
-var nameLast = alert("Enter Last Name");
+var lastName = prompt('Whats your last name');
+alert(lastName);
 
-var userBirthday = confirm("Enter Birthday"Date());
-
-var alertBirthday = alert(userBirthday);
-
-console.log(nameLast, userBirthday);
+var birthday = prompt('Whats your birthday');
+var birthdayCorrect = confirm(`Is ${birthday} your birthday`);
