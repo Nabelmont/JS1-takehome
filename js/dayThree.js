c039531fa1efeb95735153ae1c37b6f8b75d04b5
Noah <nabelmont@me.com>
2017-11-08 06:45:55 -0800

    dayThree takehome

---
+++ js/dayThree.js
@@ -0,0 +1,36 @@
+let name = prompt('Please Enter Your Name!');
+console.log(name);
+let color = prompt("What's Your Favorite Color?", red, orange, yellow, green, blue, pink, purple, black, white, and grey.);
+
+switch (color) {
+  case red:
+    prompt("Red is an Awesome color!!")
+    break;
+  case orange:
+    prompt("Personally Orange isnt my favorite color.")
+    break;
+  case yellow:
+    prompt("Yellow is a great choice!")
+    break;
+  case green:
+    prompt("Green is pretty cool!")
+    break;
+  case blue:
+    prompt("Blue is my favorite color!!!")
+    break;
+  case pink:
+    prompt("Only Cool guys wear pink.")
+    break;
+  case purple:
+    prompt("Barney is this color.")
+    break;
+  case black:
+    prompt("This is all the colors mixed together.")
+    break;
+  case white:
+    prompt("The purest color")
+    break;
+  case grey:
+    prompt("This color is right in the middle")
+    break;
+}
