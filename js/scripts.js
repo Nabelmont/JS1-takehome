c288dad2a0744d040ad8fb2d024de2244de2c6c6
Noah <nabelmont@me.com>
2017-11-13 18:28:30 -0800

    day four push

--- js/scripts.js
+++ js/scripts.js
@@ -1,2 +1,22 @@
-var message = "I hope to learn javascript in this class, then take the angular class. I would also like so learn react.js, because I am interested in builiding ios applications";
-console.log(message);
+var todos = [];
+for(let i = 0; i <= 3; i++) {
+  let todo = [];
+  var userTodo = prompt('What is something you want to get done?', 'Netflix');
+  var dueDate = prompt('How long should this take you?', '1');
+  dueDate = parseInt(dueDate);
+  if(dueDate == NaN) {
+    alert(' Oh Noooooooo');
+  } else {
+    todo.push(userTodo, dueDate);
+    todos.push(userTodo);
+  }
+    if(todos.length == 3) {
+    break;
+  } else {
+    continue;
+  }
+}
+    let v = 0;
+    while(v < 2) {
+      todos[v][1]
+}
