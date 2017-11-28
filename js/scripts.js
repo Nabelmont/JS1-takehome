a9e3f893c546cba8d4a10d104f94e4a4db8a7118
Noah <nabelmont@me.com>
2017-11-27 18:12:24 -0800

    Still trying to understand this

--- js/scripts.js
+++ js/scripts.js
@@ -1,2 +1,32 @@
-var message = "I hope to learn javascript in this class, then take the angular class. I would also like so learn react.js, because I am interested in builiding ios applications";
-console.log(message);
+// // Get all the keys from document
+// var keys = document.querySelectorAll('#calculator span');
+// var operators = ['+', '-', 'x', '÷'];
+// var decimalAdded = false;
+//
+// // Add onclick event to all the keys and perform operations
+// for(var i = 0; i < keys.length; i++) {
+// 	keys[i].onclick = function(e) {
+// 		// Get the input and button values
+// 		var input = document.querySelector('.screen');
+// 		var inputVal = input.innerHTML;
+// 		var btnVal = this.innerHTML;
+//
+// 		// If eval key is pressed, calculate and display the result
+// 		if(btnVal == '=') {
+// 			var equation = inputVal;
+// 			var lastChar = equation[equation.length - 1];
+//
+// 			// Replace all instances of x and ÷ with * and / respectively. This can be done easily using regex and the 'g' tag which will replace all instances of the matched character/substring
+// 			equation = equation.replace(/x/g, '*').replace(/÷/g, '/');
+//
+// 			// Final thing left to do is checking the last character of the equation. If it's an operator or a decimal, remove it
+// 			if(operators.indexOf(lastChar) > -1 || lastChar == '.')
+// 				equation = equation.replace(/.$/, '');
+//
+// 			if(equation)
+// 				input.innerHTML = eval(equation);
+//
+// 			decimalAdded = false;
+// 		}
+//
+// }
